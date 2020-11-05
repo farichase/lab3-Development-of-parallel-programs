01b0970cd4d8e509e65bf3bb144aaf1e827b3520
farichase <fbazartinova@mail.ru>
2020-11-05 20:22:21 +0300

    Scripted auto-commit on change (2020-11-05 20:22:21) by gitwatch.sh

--- lab3/src/main/java/javaproject/FlightsApp.java
+++ lab3/src/main/java/javaproject/FlightsApp.java
@@ -1,5 +1,10 @@
 package javaproject;
 
 public class FlightsApp {
-    public 
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.exit(-1);
+        }
+        
+    }
 }
