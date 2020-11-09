0193b335cc9fa8281de98454f5153167509fa10e
farichase <fbazartinova@mail.ru>
2020-11-09 22:48:24 +0300

    Scripted auto-commit on change (2020-11-09 22:48:24) by gitwatch.sh

--- lab3/src/main/java/javaproject/AirportSerializable.java
+++ lab3/src/main/java/javaproject/AirportSerializable.java
@@ -14,6 +14,31 @@ public class AirportSerializable implements Serializable {
     public AirportSerializable(int originAirportId, int destAirportId, float delayTime, boolean isCancelled){
         this.originAirportId = originAirportId;
         this.destAirportId = destAirportId;
-        
+        this.delayTime = delayTime;
+        this.isCancelled = isCancelled;
+    }
+    public void setDestAirportId(int destAirportId) {
+        this.destAirportId = destAirportId;
+    }
+    public void setCancelled(boolean cancelled) {
+        isCancelled = cancelled;
+    }
+    public float getDelayTime() {
+        return delayTime;
+    }
+    public int getDestAirportId() {
+        return destAirportId;
+    }
+    public int getOriginAirportId() {
+        return originAirportId;
+    }
+    public void setOriginAirportId(int originAirportId) {
+        this.originAirportId = originAirportId;
+    }
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+    public void setDelayTime(float delayTime) {
+        this.delayTime = delayTime;
     }
 }
