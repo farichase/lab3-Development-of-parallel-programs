326fd7a8f45c1fc62a88f5797aa23d4f304494be
farichase <fbazartinova@mail.ru>
2020-11-10 22:49:42 +0300

    Scripted auto-commit on change (2020-11-10 22:49:41) by gitwatch.sh

--- lab3/src/main/java/javaproject/FlightsApp.java
+++ lab3/src/main/java/javaproject/FlightsApp.java
@@ -4,9 +4,11 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
+import java.util.Iterator;
 import java.util.Map;
 
 public class FlightsApp {
@@ -36,7 +38,15 @@ public class FlightsApp {
                 new AirportSerializable(originAirportId, destAirportId, delay, isCancelled)
         );
     }
-
+    Function2 removeHeader = new Function2<Integer, Iterator<String>, Iterator<String>>() {
+        @Override
+        public Iterator<String> call(Integer ind, Iterator<String> iterator) throws Exception{
+            if (ind==0 && iterator.hasNext()) {
+                iterator.next();
+                return iterator;
+            } else return iterator;
+        }
+    };
     public static void main(String[] args) {
         if (args.length != 3) {
             System.exit(-1);
@@ -46,7 +56,9 @@ public class FlightsApp {
         String flights = args[0];
         String airports = args[1];
         String output = args[2];
-        JavaRDD<String> flightsFile = sc.textFile(flights);
+        JavaRDD<String> flightsFile = sc
+                .textFile(flights)
+                .mapPartitionsWithIndex(removeHeader, false);
         JavaRDD<String> airportsFile = sc.textFile(airports);
 
 
