4967f961fb12a96e30d93b0164630de3edb703f6
farichase <fbazartinova@mail.ru>
2020-11-10 12:49:40 +0300

    Scripted auto-commit on change (2020-11-10 12:49:40) by gitwatch.sh

--- lab3/src/main/java/javaproject/FlightsApp.java
+++ lab3/src/main/java/javaproject/FlightsApp.java
@@ -57,7 +57,7 @@ public class FlightsApp {
                 .combineByKey(
                         item -> new FlightSerializable(item.getDelayTime(),
                                 item.getDelayTime() > ZERO_DELAY ? 1 : 0,
-                                item.getIsCancelled() )
+                                item.getIsCancelled() == 1.f ? 1 : 0)
                 )
 
     }
