586797d3097070d66cf1cceb92542b8d9314dbef
farichase <fbazartinova@mail.ru>
2020-11-10 11:25:31 +0300

    Scripted auto-commit on change (2020-11-10 11:25:31) by gitwatch.sh

--- lab3/src/main/java/javaproject/FlightSerializable.java
+++ lab3/src/main/java/javaproject/FlightSerializable.java
@@ -10,5 +10,27 @@ public class FlightSerializable implements Serializable {
 
     public FlightSerializable(){
     }
-
+    public FlightSerializable(int maxDelay, int delayFlights, int cancelledFlights){
+        this.maxDelay = maxDelay;
+        this.delayFlights = delayFlights;
+        this.cancelledFlights = cancelledFlights;
+    }
+    public void setMaxDelay(int maxDelay) {
+        this.maxDelay = maxDelay;
+    }
+    public void setDelayFlights(int delayFlights) {
+        this.delayFlights = delayFlights;
+    }
+    public void setCancelledFlights(int cancelledFlights) {
+        this.cancelledFlights = cancelledFlights;
+    }
+    public int getMaxDelay() {
+        return this.maxDelay;
+    }
+    public int getDelayFlights() {
+        return this.delayFlights;
+    }
+    public int getCancelledFlights() {
+        return this.cancelledFlights;
+    }
 }
